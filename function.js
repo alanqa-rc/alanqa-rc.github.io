ae8a7f805a841903226e62f438f0027f1f29deaf
alanqa-rc <85171151+alanqa-rc@users.noreply.github.com>
2021-06-05 16:40:48 +0800

    导航页JavaScript

--- function.js
+++ function.js
@@ -1,15 +1,24 @@
 			function hpshow(x){
 				x.style.display = 'block';
+				hpwz.style.display='none';				
 			}
 			function hphidden(x){
 				x.style.display = 'none';
+				hpwz.style.display = 'block';
 			}
-			pageicons.style.display = 'none';
 
 			function scshow(x){
 				x.style.display = 'block';
 			}
 			function schidden(x){
 				x.style.display = 'none';
+			}
+			window.onload=function(){
+				pageicons.style.display = 'none';
+				hpwz.style.display = 'block';
+				sin.style.display = 'none';
+				console.log("感谢使用阿兰卡导航页！")
+				console.log("作者QQ:30087370")
+				console.log("2021 6 5 15:45")
 			}
 			
