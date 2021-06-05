db1ed7410a3fccc8788f7482f612669a94f365c3
alanqa-rc <85171151+alanqa-rc@users.noreply.github.com>
2021-06-05 11:39:53 +0800

    Add files via upload

---
+++ function.js
@@ -0,0 +1,15 @@
+			function hpshow(x){
+				x.style.display = 'block';
+			}
+			function hphidden(x){
+				x.style.display = 'none';
+			}
+			pageicons.style.display = 'none';
+
+			function scshow(x){
+				x.style.display = 'block';
+			}
+			function schidden(x){
+				x.style.display = 'none';
+			}
+			
