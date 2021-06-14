a0e61a25690b1de6f9e16cd8264786956dc30c99
alanqa-rc <85171151+alanqa-rc@users.noreply.github.com>
2021-06-14 22:17:11 +0800

    起始页js20210614

--- function.js
+++ function.js
@@ -7,25 +7,22 @@
 				hpwz.style.display = 'block';
 			}
 
-			function scshow(x){
-				x.style.display = 'block';
+			function scshow(){
+				sdiv.style.left='20%';
+				sdiv.style.right='20%';
+				sdiv.style.background='rgba(255,255,255,0.75)';
+			}
+			function schidden(){
 				sdiv.style.left='38%';
 				sdiv.style.right='38%';
 				sdiv.style.background='rgba(255,255,255,0)';
 			}
-			function schidden(x){
-				x.style.display = 'none';
-				sdiv.style.left='48%';
-				sdiv.style.right='48%';
-				sdiv.style.background='rgba(255,255,255,1)';
-			}
 			window.onload=function(){
 				pageicons.style.display = 'none';
 				hpwz.style.display = 'block';
-				schidden(sin);
-				stobing();
+				schidden();
 				console.log("感谢使用阿兰卡导航页！")
 				console.log("作者QQ:30087370")
-				console.log("2021 6 5 15:45")
+				console.log("2021 6 14 18:53")
 			}
 			
